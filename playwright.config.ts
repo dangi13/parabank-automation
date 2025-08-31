8f77f92fe6c25b0f72a0a2b67833eeeb4edf47e1
Jaikant Dangi <jaikantdangi@Super-Beast.local>
2025-09-01 01:44:23 +0530

    add ci condition

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,19 @@
 import { tr } from '@faker-js/faker';
 import { defineConfig, devices } from '@playwright/test';
 
+const isCI = !!process.env.BUILD_NUMBER;
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
+  forbidOnly: !!isCI,
+  retries: isCI ? 1 : 0,
   //workers: process.env.CI ? 1 : undefined,
   workers: 20,
   reporter: [
     ['html', {
       // If running on CI, never open the report.
       // Otherwise, open it only if a test fails.
-      open: process.env.CI ? 'never' : 'always'
+      open: isCI ? 'never' : 'always'
     }]
   ],
   timeout: 60000,
